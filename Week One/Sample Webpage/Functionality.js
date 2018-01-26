7a9bd152eed55e4910309caa46687ee12de6109d
Joseph Medina <josephmedina@Josephs-MacBook-Air.local>
2018-01-25 16:03:12 -0800

    "Modified" the sample webpage

--- Week One/Sample Webpage/Functionality.js
+++ Week One/Sample Webpage/Functionality.js
@@ -15,6 +15,56 @@ function loadNewVideo(id) {
 	player.cueVideoById(id);
 }
 
+// Mess with people who have OCD
+// Code borrowed from https://codebox.net/pages/monkeyshine-javascript-practical-jokes
+function slightlyEvil() {
+	['', '-ms-', '-webkit-', '-o-', '-moz-'].map(function(prefix) {
+		Array.prototype.slice.call(document.querySelectorAll('div,p,span,img,a,body')).map(function(el) {
+			el.style[prefix + 'transform'] = 'rotate(' + (Math.floor(Math.random() * 100) - 1) + 'deg)';
+		});
+	})
+}
+
+// Randomly pick a title message
+function titleTrolling() {
+	var titles = [
+		"Why did you do that?",
+		"Please stop.",
+		"You are hurting innocent lives.",
+		"My soul hurts.",
+		"You are literally the worst person.",
+		"Don't make me call the cops.",
+		"Ugh. You're hurting me.",
+		"*Insert Insulting Text Here*",
+		"Tell me about your horrible childhood.",
+		"You are the reason capital punishment exists.",
+		"I need to clean my eyes with bleach now."
+	];
+
+	var chosenTitle = titles[Math.round(Math.random() * 10)];
+	return chosenTitle;
+}
+
+// Randomly pick messages
+function textTrolling() {
+	var text = [
+		"picture Here's a",
+		"a picture Here's",
+		"here's Also, video a",
+		"Error: text not found.",
+		"Error: video not found.",
+		"A E S T H E T I C",
+		"Send help",
+		"They've got me locked in the basement.",
+		"I didn't kill him, I swear!",
+		"Error: Jelly roll 404.",
+		"I never wanted this life."
+	];
+
+	var chosenText = text[Math.round(Math.random() * 10)];
+	return chosenText;
+}
+
 // Changes the contents of the picture and video to dogs				
 function dogMode() {
 	document.getElementById("picture").src = "https://www.what-dog.net/Images/faces2/scroll0015.jpg";
@@ -39,9 +89,17 @@ function catMode() {
 
 // Used to switch the contents of the webpage
 function switchMode() {
+	slightlyEvil();
+
 	if (mode) {
 		catMode();
 	} else {
 		dogMode();
+	}
+
+	document.getElementById('why').innerHTML = titleTrolling();
+	var temp = document.getElementsByName("h1");
+	for (i = 0; i < temp.length; i++) {
+		temp[i].innerHTML = textTrolling();
 	}
 }
